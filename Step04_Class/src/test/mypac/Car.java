bfcc13be9d7ab7d23250736bf2de933c000c244b
kang-heeeun <asd8186@naver.com>
2021-05-03 17:34:04 +0900

    Step04 에 MyMain01 클래스 추가함

--- Step04_Class/src/test/mypac/Car.java
+++ Step04_Class/src/test/mypac/Car.java
@@ -10,10 +10,11 @@ package test.mypac;
 
 public class Car {
 	//자동차의 이름을 저장할 필드 
-	public String name;
+	public String name; // field
 	
 	//달리는 메소드
 	public void drive() {
+		String name = null; // local variable;
 		System.out.println("부릉 부릉 달려요~");
 	}
 }
